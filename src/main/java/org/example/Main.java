6cb21b656e354382ef733da02ada6b6c23022bd6
AJ Cave <acave@cj.com>
2014-04-03 10:12:16 -0700

    Changed bad main class name to more appropriate class name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
-public class HelloWorld {
+public class Main {
 
    public static void main(String[] args) throws Exception {
       Server server = new Server(8080);
